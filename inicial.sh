ae73edc47fbedf94800a2a304f5e03e7fdb74eae
Diego <casagrandabr@gmail.com>
2017-11-23 11:01:18 -0200

    ConfInicial

---
+++ inicial.sh
@@ -0,0 +1,22 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "#######################"
+echo " Atualizando Programas "
+echo "#######################"
+
+termux-setup-storage
+
+apt update
+apt upgrade
+
+echo "##########################"
+echo " Intalando Programas base "
+echo "##########################"
+
+apt install curl
+apt install figlet 
+apt install git
+apt install nano
+
+echo figlet "FSOCIETY" >> /data/data/com.termux/files/usr/etc/bash.bashrc 
+echo figlet echo -e "\033[1m \e[32m   Bem Vindo!" >> /data/data/com.termux/files/usr/etc/bash.bashrc 
